de0d1f2a25f5b40decaee149639740fd08b283ec
Leandro Da Souza <dasouza.leandro@gmail.com>
2023-02-05 15:10:16 +0100

    day 16

---
+++ 16/example.py
@@ -0,0 +1,32 @@
+# import turtle
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# timmy.shape("turtle")
+# timmy.color("burlywood", "DarkSlateGray")
+# timmy.shapesize(1.5, 1.5, 3)
+# turtle.forward(100)
+# my_screen = Screen()
+#
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column('Pokemon Name', [
+    'Pikachu',
+    'Squirtle',
+    'Charmander'
+])
+
+table.add_column('Type', [
+    'Electric',
+    'Water',
+    'Fire'
+])
+
+table.align = 'l'
+
+print(table)
